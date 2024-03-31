8e6c2880d342a334c87d226478097f7205f81b77
AlvareitorHD <alvareitor.14.03@gmail.com>
2024-03-31 21:10:32 +0200

    Finalizado P2

--- P2/README.txt
+++ P2/README.txt
@@ -1,10 +1,10 @@
 ***********************************************************************
 ** Álvaro Ruiz Luzón *************************  SIBW  *****************
 ***********************************************************************
-***********   Práctica 1: HTML y CSS   ********************************
+***********   Práctica 2: JavaScript y Bootstrap   ********************
 ***********************************************************************
 
-Ficheros HTML:
+Ficheros HTML y CSS de la Práctica 1:
 
  - portada: página principal de las actividades de montaña. El
             contenedor de actividades puede tener cualquier
@@ -33,11 +33,27 @@ Ficheros HTML:
  			 con un estilo diferente y adecuado para
  			 imprimir la actividad.
  
-Ficheros CSS:
-
 		- estilo_por: hoja de estilos para la portada.
 		- estilo_act: hoja de estilos para cada actividad.
 		- estilo_imp: hoja de estilos casi igual que estilo_act pero
 						  con muchas cosas desactivadas (como la cabecera y
 						  espacio auxiliar) y cambios en la tipografía para
 						  una mejor impresión a papel.
+
+Mejoras Práctica 2:
+
+ - En esta práctica he intentado aplicar contenedores responsive y más dinámicos con Bootstrap, además
+ de haber mejorado un par de cosas de la práctica anterior (como una tipografía nueva, cabecera mejorada, botón imprimir y márgenes).
+
+ - Comentarios:
+	Como nueva funcionalidad se ha implementado la sección de Comentarios en cada actividad, que se despliega mediante el botón
+	"Comentarios". Al abrir esta sección ya hay dos comentarios predefinidos.
+
+	Cada comentario tiene foto de usuario, nombre, hora y fecha del comentario como cabecera de éste. También tiene su reseña (texto breve
+	sobre la experiencia de la actividad o similar).
+ 	
+	· Formulario de comentario: el usuario puede añadir un comentario suyo nuevo. Para ello deberá dar su nombre, email y reseña.
+	Si estos campos son válidos, se añadirá su comentario, por el contrario no se añadirá y se avisará con su correspodientes avisos.
+
+	* También hay un pequeño mecanismo de censura de palabras prohibidas (como insultos u otras palabras no permitidas), que sustituye
+	dicha palabra por la primera letra y asteriscos (por ej: prado -> p**** ).
--- P2/js/script.js
+++ P2/js/script.js
@@ -5,9 +5,12 @@ const resenia_usr = document.getElementById("input_resena");
 const nombre_usr = document.getElementById("name_input");
 const email_usr = document.getElementById("exampleInputEmail1");
 
+/* Variable que controla la rotación de foto de perfil por defecto (0-3) */
 let usr = 0;
 
+/* Funcion para crear comentario dandole el nombre y reseña, la fecha y hora es automática */
 function crearComentario(nombre,resenia){
+
     var comentario = document.createElement("div");
     comentario.classList.add("comentario");
     var cabecera = document.createElement("header");
@@ -49,10 +52,13 @@ function crearComentario(nombre,resenia){
     return comentario;
 }
 
+/* Función que comprueba si el comentario es válido tanto por nombre, email y reseña */
 function esValido(nombre,email,resena){
     var valido = true;
     var errorEmail = document.getElementById("emailError");
     var errorNombre = document.getElementById("errorName");
+    var errorResena = document.getElementById("resenaError");
+
     if(nombre ==''){
         valido = false;
         errorNombre.classList.remove("oculto");
@@ -61,7 +67,16 @@ function esValido(nombre,email,resena){
         errorNombre.classList.add("oculto");
         errorNombre.classList.remove("visible");
     }
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    /* Patron email:
+        ^ inicio
+        [...]+ comprueba que haya de a-z o A-Z o 0-9 o . _ - (pero al menos un caracter)
+        @ obligatorio
+        [...]+ igual que lo anterior
+        \. obligatorio un punto
+        [...]{2,} el tld (top-level domain o dominio nivel superior) debe ser letras y como mínimo 2 caracteres
+        $ fin
+    */
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if(emailPattern.test(email) == false){
         valido = false;
         errorEmail.classList.remove("oculto");
@@ -70,32 +85,53 @@ function esValido(nombre,email,resena){
         errorEmail.classList.add("oculto");
         errorEmail.classList.remove("visible");
     }
+    if(resena == ''){
+        valido = false;
+        errorResena.classList.remove("oculto");
+        errorResena.classList.add("visible");
+    }else{
+        errorResena.classList.add("oculto");
+        errorResena.classList.remove("visible");
+    }
     return valido;
 }
 
+/* Evento botón Subir Comentario: subirá el comentario a la sección si es válido */
 botonenvio.addEventListener('click',() => {
-    if(esValido(nombre_usr.value,email_usr,resenia_usr.value)){
+    if(esValido(nombre_usr.value,email_usr.value,resenia_usr.value)){
         let comentario = crearComentario(nombre_usr.value,resenia_usr.value);
         comentarios.appendChild(comentario);
     }
 });
 
+/* Evento botón Comentarios para mostrar/ocultar la sección de comentarios */
 botoncom.addEventListener('click', () => {
     let com = document.querySelector('#comentarios');
     if(com.classList.contains("oculto")){
         com.classList.remove("oculto");
         com.classList.add("visible");
+        
     } else{
         com.classList.remove("visible");
         com.classList.add("oculto");
     }
 });
 
-const prohibidas = ["puta","tonto","gilipollas","petardo","swad","etc"];
+/* Palabras prohibidas */
+const prohibidas = ["puta","tonto","gilipollas","petardo","prado","ugr","mierda","lol","feo"];
 
 resenia_usr.addEventListener('keyup', () =>{
     let valor = resenia_usr.value;
-    for(var i = 0; i < prohibidas.length;i++)
-    valor = valor.replace(/\b{prohibid}\b/gi, "p***");
+    for (var i = 0; i < prohibidas.length; i++) {
+        /* Creamos una variable regex que busque la palabra prohibida (gi: g de global en todo el texto e i para mayusculas
+            y minusculas, \\b indica límite de palabra */
+        var regex = new RegExp("\\b" + prohibidas[i] + "\\b", "gi");
+        /* Reemplazo las palabras prohibidas por su primera letra y asteriscos */
+        var censura = prohibidas[i].charAt(0);
+        for(var j = 1; j < prohibidas[i].length; j++){
+            censura += '*';
+        }
+        valor = valor.replace(regex, censura);
+    }
     resenia_usr.value = valor;
 });
